06e7c54641542c8cc16314ab0e2d719834beb324
LucaSs55 <lucassantos2018d@gmail.com>
2026-09-16 14:44:08 -0300

    Initial Commit

---
+++ src/main/java/com/tracebank/TracebankApplication.java
@@ -0,0 +1,13 @@
+package com.tracebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracebankApplication.class, args);
+	}
+
+}
